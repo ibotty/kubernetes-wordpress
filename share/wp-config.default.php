c90f249795c3e058c208f8805b783f23d6e546f9
Tobias Florek <tob@butter.sh>
2016-06-29 08:59:12 +0200

    fix typo in DB_CHARSET

--- share/wp-config.default.php
+++ share/wp-config.default.php
@@ -32,7 +32,7 @@ define('DB_PASSWORD', '');
 define('DB_HOST', 'wordpress-db');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf-8');
+define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
