cc2df667bf0f695b4434dcb4b78b8f4f9cdd5228
Tobias Florek <tob@butter.sh>
2016-09-01 16:14:58 +0200

    remove tabs, use whitespaces

--- share/wp-config.default.php
+++ share/wp-config.default.php
@@ -89,7 +89,7 @@ if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROT
 
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+    define('ABSPATH', dirname(__FILE__) . '/');
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
